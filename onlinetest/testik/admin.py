7976d3fa2a1fb3bc5134a1006340489ecec54900
Polina Rymorenko <rymorenkop@gmail.com>
2024-11-26 13:23:30 +0300

    add functions in the admin panel

--- onlinetest/testik/admin.py
+++ onlinetest/testik/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
 from .models import Test, Category
 
-
 @admin.register(Test)
 class TestAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    fields = ['title','slug', 'content', 'cat', 'tags']
+    prepopulated_fields = {"slug": ("title", )}
+    filter_horizontal = ['tags']
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
     ordering = ['time_create', 'title']
-    list_editable = ('is_published', 'cat' )
+    list_editable = ('is_published', 'cat')
     list_per_page = 10
+    actions = ['set_published', 'set_draft']
+    search_fields = ['title', 'cat__name']
+    list_filter = ['cat__name', 'is_published']
+
+    @admin.display(description="Краткое описание", ordering='content')
+    def brief_info(self, obj):
+        return f"Описание {len(obj.content)} символов"
+
+    def set_published(self, request, queryset):
+        queryset.update(is_published=Test.Status.PUBLISHED)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- onlinetest/testik/models.py
+++ onlinetest/testik/models.py
@@ -1,20 +1,40 @@
 from tabnanny import verbose
 
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Test.Status.PUBLISHED)
-
+def translit_to_eng(s: str) -> str:
+     d = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g',
+        'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh',
+        'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k',
+        'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
+        'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+        'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts',
+        'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ъ': '',
+        'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G',
+        'Д': 'D', 'Е': 'E', 'Ё': 'Yo', 'Ж': 'Zh',
+        'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K',
+        'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O',
+        'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T',
+        'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts',
+        'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ъ': '',
+        'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya',
+    }
+     return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
 
 class Test(models.Model):
     class Status (models.IntegerChoices):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name = "Заголовок")
+    title = models.CharField(max_length=255, verbose_name = "Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name = "Слаг")
     content = models.TextField(blank=True, verbose_name = "Тест")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name = "Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name = "Время изменения")
@@ -38,9 +58,13 @@ class Test(models.Model):
     def get_absolute_url(self):
         return reverse('test', kwargs={'test_slug': self.slug})
 
+    #def save(self, *args, **kwargs):
+       # self.slug = slugify(translit_to_eng(self.title))
+       # super().save(*args, **kwargs)
+
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name = "Категория")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name = "Слаг")
 
     class Meta:
         verbose_name = "Категория"
@@ -53,8 +77,8 @@ class Category(models.Model):
         return reverse('category', kwargs={'cat_slug': self.slug})
 
 class TagTest(models.Model):
-    tag = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    tag = models.CharField(max_length=100, db_index=True, verbose_name = "Тег")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name = "Слаг")
 
     def __str__(self):
         return self.tag
