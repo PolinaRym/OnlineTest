c7bf60692b93e87bc0aa7e0c299a7091d9569f42
Polina Rymorenko <rymorenkop@gmail.com>
2024-11-29 13:13:20 +0300

    changes in add test

--- onlinetest/testik/forms.py
+++ onlinetest/testik/forms.py
@@ -1,9 +1,19 @@
 from django import forms
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.utils.deconstruct import deconstructible
+
 from .models import Category
 
+
 class AddTestForm(forms.Form):
-    title = forms.CharField(max_length = 255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length = 255, label="URL")
+    title = forms.CharField(max_length = 255, min_length=5,
+                            label="Заголовок",
+                            widget=forms.TextInput(attrs={'class': 'form-input'}))
+    slug = forms.SlugField(max_length = 255, label="URL",
+                           validators=[
+                               MinLengthValidator(5, message="Минимум 5 символов"),
+                               MaxLengthValidator(100, message="Минимум 100 символов"),
+                           ])
     content = forms.CharField(widget=forms.Textarea(), required=False, label="Контент")
     is_published = forms.BooleanField(required=False, initial = True, label="Статус")
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
--- onlinetest/testik/models.py
+++ onlinetest/testik/models.py
@@ -1,5 +1,6 @@
 from tabnanny import verbose
 
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
@@ -34,7 +35,11 @@ class Test(models.Model):
         PUBLISHED = 1, 'Опубликовано'
 
     title = models.CharField(max_length=255, verbose_name = "Заголовок")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name = "Слаг")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name = "Слаг", validators=[
+                               MinLengthValidator(5, message="Минимум 5 символов"),
+                               MaxLengthValidator(100, message="Максимум 100 символов"),
+                           ])
+
     content = models.TextField(blank=True, verbose_name = "Тест")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name = "Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name = "Время изменения")
