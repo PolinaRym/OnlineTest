1aa3b38b72c3beaf9b70ba848e2218d6348bd53d
Polina Rymorenko <rymorenkop@gmail.com>
2024-11-29 13:29:10 +0300

    changes in forms

--- onlinetest/testik/forms.py
+++ onlinetest/testik/forms.py
@@ -2,18 +2,15 @@ from django import forms
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.utils.deconstruct import deconstructible
 
-from .models import Category
+from .models import Category, Test
 
 
-class AddTestForm(forms.Form):
-    title = forms.CharField(max_length = 255, min_length=5,
-                            label="Заголовок",
-                            widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length = 255, label="URL",
-                           validators=[
-                               MinLengthValidator(5, message="Минимум 5 символов"),
-                               MaxLengthValidator(100, message="Минимум 100 символов"),
-                           ])
-    content = forms.CharField(widget=forms.Textarea(), required=False, label="Контент")
-    is_published = forms.BooleanField(required=False, initial = True, label="Статус")
+class AddTestForm(forms.ModelForm):
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
+    class Meta:
+        model = Test
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'class': 50, 'rows': 5}),
+        }
