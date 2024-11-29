1a066e16789e68c876b6aa52b8fba1c7bd20378d
Polina Rymorenko <rymorenkop@gmail.com>
2024-11-29 12:18:48 +0300

    changes in forms

--- onlinetest/testik/forms.py
+++ onlinetest/testik/forms.py
@@ -5,5 +5,5 @@ class AddTestForm(forms.Form):
     title = forms.CharField(max_length = 255, label="Заголовок")
     slug = forms.SlugField(max_length = 255, label="URL")
     content = forms.CharField(widget=forms.Textarea(), required=False, label="Контент")
-    is_published = forms.BooleanField(required=False, label="Статус")
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории")
+    is_published = forms.BooleanField(required=False, initial = True, label="Статус")
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
