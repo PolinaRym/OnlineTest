4e68086d2ddfd4847b8c6e00fcca11c8fe077648
Polina Rymorenko <rymorenkop@gmail.com>
2024-11-29 11:58:27 +0300

    add name in addtest

--- onlinetest/testik/forms.py
+++ onlinetest/testik/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Category
 
 class AddTestForm(forms.Form):
-    title = forms.CharField(max_length = 255)
-    slug = forms.SlugField(max_length = 255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length = 255, label="Заголовок")
+    slug = forms.SlugField(max_length = 255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(), required=False, label="Контент")
+    is_published = forms.BooleanField(required=False, label="Статус")
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории")
