e3ce8d66ca6ed3b65e66e25d71737cc6c6f768b2
Polina Rymorenko <rymorenkop@gmail.com>
2024-12-01 17:00:15 +0300

    changes in images

--- onlinetest/testik/admin.py
+++ onlinetest/testik/admin.py
@@ -1,12 +1,15 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.safestring import mark_safe
+
 from .models import Test, Category
 
 @admin.register(Test)
 class TestAdmin(admin.ModelAdmin):
-    fields = ['title','slug', 'content', 'cat', 'tags']
+    fields = ['title','slug', 'content', 'photo', 'test_photo', 'cat', 'tags']
+    readonly_fields = ['test_photo']
     prepopulated_fields = {"slug": ("title", )}
     filter_horizontal = ['tags']
-    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display = ('title', 'test_photo', 'time_create', 'is_published', 'cat')
     list_display_links = ('title',)
     ordering = ['time_create', 'title']
     list_editable = ('is_published', 'cat')
@@ -14,13 +17,24 @@ class TestAdmin(admin.ModelAdmin):
     actions = ['set_published', 'set_draft']
     search_fields = ['title', 'cat__name']
     list_filter = ['cat__name', 'is_published']
+    save_on_top = True
 
-    @admin.display(description="Краткое описание", ordering='content')
-    def brief_info(self, obj):
-        return f"Описание {len(obj.content)} символов"
+    @admin.display(description="Изображение", ordering='content')
+    def test_photo(self, testik: Test):
+        if testik.photo:
+            return mark_safe(f"<img src='{testik.photo.url}' width=50>")
+        return "Без фото"
 
+    @admin.action(description="Опубликовать выбранные записи")
     def set_published(self, request, queryset):
-        queryset.update(is_published=Test.Status.PUBLISHED)
+        count = queryset.update(is_published=Test.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записей.")
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Test.Status.DRAFT)
+        self.message_user(request, f"{count} записей сняты с публикации!", messages.WARNING)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
