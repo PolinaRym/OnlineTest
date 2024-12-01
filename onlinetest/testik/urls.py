93ee6e9b6907a5f4e25c4cc318dc518157a8e1c2
Polina Rymorenko <rymorenkop@gmail.com>
2024-12-02 01:32:55 +0300

    changes add test

--- onlinetest/testik/urls.py
+++ onlinetest/testik/urls.py
@@ -6,10 +6,10 @@ register_converter(converters.FourDigitYearConverter, "year4")
 urlpatterns = [
     path('', views.TestHome.as_view(), name='home'), # http://127.0.0.1.8000
     path('about/', views.about, name='about'),
-    path('addpage/', views.AddTest.as_view(), name='add_test'),
+    path('addtest/', views.AddTest.as_view(), name='add_test'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
-    path('test/<slug:test_slug>/', views.show_test, name = 'test'),
+    path('test/<slug:test_slug>/', views.ShowPost.as_view(), name = 'test'),
     path('category/<slug:cat_slug>', views.TestCategory.as_view(), name='category'),
     path('tag/<slug:tag_slug>/', views.TagTestList.as_view(), name='tag')
 ]
--- onlinetest/testik/views.py
+++ onlinetest/testik/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, FormView
 
 from .forms import AddTestForm, UploadFileForm
 from .models import Test, Category, TagTest, UploadFiles
@@ -14,16 +14,6 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'},
 ]
-
-# def index(request):
-#     tests = Test.published.all().select_related('cat')
-#     data = {
-#         'title': 'Главная страница',
-#             'menu': menu,
-#             'tests': tests,
-#              'cat_selected': 0,
-#              }
-#     return render(request, 'testik/index.html', context=data )
 
 class TestHome(ListView):
     template_name = 'testik/index.html'
@@ -48,56 +38,33 @@ def about(request):
     return render(request, 'testik/about.html',
                   {'title': 'О сайте','menu': menu, 'form': form})
 
-def show_test(request, test_slug):
-    test = get_object_or_404(Test, slug=test_slug)
-    data = {
-        'title': test.title,
+class ShowPost(DetailView):
+    # model = Women
+    template_name = 'testik/test.html'
+    slug_url_kwarg = 'test_slug'
+    context_object_name = 'test'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['test'].title
+        context['menu'] = menu
+        return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Test.published, slug=self.kwargs[self.slug_url_kwarg])
+
+class AddTest(FormView):
+    form_class = AddTestForm
+    template_name = 'testik/addtest.html'
+    success_url = reverse_lazy('home')
+    extra_context = {
         'menu': menu,
-        'test': test,
-        'cat_selected': 1,
+        'title': 'Создание теста',
     }
-    return render(request, 'testik/test.html', data)
 
-class AddTest(View):
-    def get(self, request):
-        form = AddTestForm
-        data = {
-            'menu': menu,
-            'title': 'Создание теста',
-            'form': form
-        }
-        return render(request, 'testik/test.html', data)
-
-    def post(self, request):
-        form = AddTestForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-        data = {
-            'menu': menu,
-            'title': 'Создание теста',
-            'form': form
-        }
-        return render(request, 'testik/test.html', data)
-
-
-
-# def addtest(request):
-#     if request.method == 'POST':
-#        form = AddTestForm(request.POST, request.FILES)
-#        if form.is_valid():
-#            form.save()
-#            return redirect('home')
-#     else:
-#         form = AddTestForm()
-#
-#     data = {
-#         'menu': menu,
-#         'title': 'Добавление теста',
-#         'form': form
-#
-#     }
-#     return render(request, 'testik/addtest.html', data)
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -136,18 +103,6 @@ class TestCategory(ListView):
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
-# def show_tag_testlist(request, tag_slug):
-#     tag =  get_object_or_404(TagTest, slug=tag_slug)
-#     tests = tag.tags.filter(is_published=Test.Status.PUBLISHED)
-#
-#     data = {
-#         'title': f"Тег: {tag.tag}",
-#         'menu': menu,
-#         'tests': tests,
-#         'cat_selected': None,
-#     }
-#
-#     return render(request, 'testik/index.html', context=data)
 class TagTestList(ListView):
     template_name = 'testik/index.html'
     context_object_name = 'tests'
