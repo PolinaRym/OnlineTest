044f61e94af392172b7be3a60546f055bf6a72f5
Polina Rymorenko <rymorenkop@gmail.com>
2024-12-02 15:44:47 +0300

    add pagination

--- onlinetest/testik/views.py
+++ onlinetest/testik/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -26,15 +27,13 @@ class TestHome(DataMixin, ListView):
         return Test.published.all().select_related('cat')
 
 def about(request):
-    if request.method == 'POST':
-       form = UploadFileForm(request.POST, request.FILES)
-       if form.is_valid():
-           fp = UploadFiles(file=form.cleaned_data['file'])
-           fp.save()
-    else:
-       form = UploadFileForm()
+    contact_list = Test.published.all()
+    paginator = Paginator(contact_list, 5)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(request, 'testik/about.html',
-                  {'title': 'О сайте', 'form': form})
+                  {'title': 'О сайте', 'page_obj': page_obj})
 
 class ShowTest(DataMixin, DetailView):
     template_name = 'testik/test.html'
