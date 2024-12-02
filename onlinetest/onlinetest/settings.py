f73032a01fc96cdf028b1e38a6f544cde0366b60
Polina Rymorenko <rymorenkop@gmail.com>
2024-12-03 01:57:34 +0300

    add telegram channel

--- onlinetest/onlinetest/settings.py
+++ onlinetest/onlinetest/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS, LOGIN_REDIRECT_URL
+from django.conf.global_settings import STATICFILES_DIRS, LOGIN_REDIRECT_URL, AUTHENTICATION_BACKENDS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'users:login'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBacked',
+]
