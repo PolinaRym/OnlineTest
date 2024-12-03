d7b1b7d10cab49f4bbadc0bccc47fc1e4f26c4c0
Polina Rymorenko <rymorenkop@gmail.com>
2024-12-03 13:24:07 +0300

    add ModelBackend

--- onlinetest/onlinetest/settings.py
+++ onlinetest/onlinetest/settings.py
@@ -143,5 +143,6 @@ LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'users:login'
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBacked',
+    'django.contrib.auth.backends.ModelBackend',
+    'users.authentication.EmailAuthBackend',
 ]
